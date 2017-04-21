88a069a1800435a27f1568c1eb0c16198dec35b8
J. Williamn <hwilliamnq@gmail.com>
2017-04-21 18:02:57 -0500

    small changes,

--- extraction/FormatModel/UtilDebug.py
+++ extraction/FormatModel/UtilDebug.py
@@ -13,8 +13,11 @@
 
 import numpy as np
 import cv2
-import modeling
 from matplotlib import pyplot as plt
+from PIL import Image
+import sys
+
+import modeling
 
 
 def plotearCategoriasPosicionesImagenes(img, cat_pos, cat_img):
@@ -27,7 +30,13 @@ def plotearCategoriasPosicionesImagenes(img, cat_pos, cat_img):
             plotearCategoriasPosicionesImagenes(img, st, cat_img[st.name])
 
 def plotear(img, position, arrayOfImages, countItems, arrayPredictedValues):
+    #arg = sys.argv[1]
+    #background = Image.open(arg)
     for k in range(countItems):
         pixel_y = (position[0][1] + position[1][1])/2
         pixel_x = (position[0][0]*(countItems-k) + position[1][0]*k)/ countItems
         print('ploteando la imagen ',k, ' en ', position, ' valor predicho: ', arrayPredictedValues[k])
+        #print('pixel Y', pixel_y)
+        #print('pixel X', pixel_x)
+        #background.paste(arrayOfImages[k], position, arrayOfImages[k])
+        #background.show()
